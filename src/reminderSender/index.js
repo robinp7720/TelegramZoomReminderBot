3f7a1b872979c95822e6841cb691f8a413fa3ee6
Zeyphros <robin@decker.cx>
2021-04-12 15:11:38 +0200

    Add zoom id to message

--- src/reminderSender/index.js
+++ src/reminderSender/index.js
@@ -6,6 +6,12 @@ export const reminderSender = async function (lecture) {
     console.log('Sending reminder for', lecture.name);
 
     let message = `${lecture.name} startet in 5 Minuten.\nhttps://zb.robindecker.me/${lectures.indexOf(lecture)}`;
+
+    // Add optional information to message such as Webinar-IDs and Passwords
+    // These don't need to be specified in the lectures.json file, but if they are
+    // It is beneficial to show them.
+    if (lecture.zoomid)
+        message += `\nWebinar-ID: ${lecture.zoomid}`;
     if (lecture.password)
         message += `\nKenncode: ${lecture.password}`;
 
