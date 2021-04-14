aea7f140fa6995fe1fd41a04ad680555dd789359
Zeyphros <robin@decker.cx>
2021-04-14 23:30:04 +0200

    Add additional pre-programmed response to message

--- src/commandHandlers/zoomyResponder.js
+++ src/commandHandlers/zoomyResponder.js
@@ -6,4 +6,7 @@ export default ctx => {
     if (text.includes("ich hab dich lieb"))
         return ctx.reply('Ich hab dich auch lieb 😘', {'reply_to_message_id': ctx.message.message_id});
 
+    if (text.includes("ist das wahr"))
+        return ctx.reply('Nein', {'reply_to_message_id': ctx.message.message_id});
+
 }
