f3856785ae6a686fa4549f705f73c713dcd4c0bd
Zeyphros <robin@decker.cx>
2021-04-19 23:54:15 +0200

    Revert "redirect on short link"
    This reverts commit 3b285afe2e00a1142fd2c1b727580046e1a12b84.

--- src/zoomProxy/index.js
+++ src/zoomProxy/index.js
@@ -13,5 +13,20 @@ zoomProxyServer.get('/:id', (req, res, next) => {
 
     if (!lectures[id]) return next();
 
-    res.redirect(lectures[id].url, next);
+    // TODO: Move this to a separate file and implement a view rendering library
+    res.sendRaw(200, `
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="Refresh" content="0; url='${lectures[id].url}'" />
+            
+            <title>${lectures[id].name}</title>
+            <meta property="og:site_name" content="${lectures[id].name}">
+        </head>
+        <body>
+            You should be redirected shortly.
+            If not, press <a href="${lectures[id].url}">here</a>
+        </body>
+    </html>
+    `);
 })
