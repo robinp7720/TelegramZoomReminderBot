bcee2195be43f0026d1db653e89b1a7cdf5ab9ca
Zeyphros <robin@decker.cx>
2021-04-19 11:51:07 +0200

    Sort the lectures by time and filter out non channel relevant lectures

--- src/commandHandlers/lectures.js
+++ src/commandHandlers/lectures.js
@@ -3,6 +3,8 @@ import {capitalize} from '../utils/text';
 import {days, getDay} from '../utils/days';
 
 export default async ctx => {
+    const channelID = (await ctx.getChat()).id;
+
     let input = ctx.message.text.split(' ');
     let sortDate = -1;
 
@@ -12,10 +14,24 @@ export default async ctx => {
 
     let message = "";
 
-    for (const lecture of lectures) {
-        if (sortDate > -1 && parseInt(lecture.day) !== sortDate)
-            continue;
+    // We only want to show lectures relevant to the channel where the command was executed
+    const filteredLectures = lectures.filter((a) => {
+        // Filter out all lectures not happening on the day specified
+        if (sortDate > -1 && parseInt(a.day) !== sortDate)
+            return false;
 
+        // Filter out all lectures on for the current channel
+        return a.channels.indexOf(channelID) !== -1;
+    });
+
+    // To improve the readability we'll also sort the lectures by date and time
+    filteredLectures.sort((a, b) => {
+        const a_time = (a.day * 60 * 24) + (a.time[0] * 60 + a.time[1]);
+        const b_time = (b.day * 60 * 24) + (b.time[0] * 60 + b.time[1]);
+        return a_time - b_time;
+    });
+
+    for (const lecture of filteredLectures) {
         message += `${lecture.name} - ${lecture.time[0]}:${lecture.time[1]} - ${capitalize(days[lecture.day])}\n`
     }
 
