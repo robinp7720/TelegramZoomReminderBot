8a10409f98437cfd6a6e0c56ff6bb872b2fe0dda
Zeyphros <robin@decker.cx>
2021-04-20 11:54:28 +0200

    More semantic information It doesn't actually show any of the info in zoom for some reason.. but maybe it'll randomly start working

--- src/zoomProxy/index.js
+++ src/zoomProxy/index.js
@@ -19,16 +19,25 @@ zoomProxyServer.get('/:id', (req, res, next) => {
     // We don't really want that because the information is useless to us and it fills up the telegram chat
     // very quickly
     res.sendRaw(200, `
+    <!DOCTYPE html>
     <html lang="en">
         <head>
-            <meta charset="UTF-8">
-            
+            <meta charset="utf-8">
+                       
+            <meta name="referrer" content="origin-when-cross-origin">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1">
+            <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0">
             <title>${lectures[id].name}</title>
-            
-            <meta property="og:site_name" content="${lectures[id].name}">
+            <meta name="keywords" content="Learning, test, hello, fuckyou">
+            <meta name="robots" content="noindex,nofollow">
+            <meta property="og:type" content="activity">
             <meta property="og:title" content="${lectures[id].name}">
-            
-            <meta http-equiv="Refresh" content="0; url='${lectures[id].url}'" />
+            <meta property="og:description" content="This zoom lecture will start in 5 mins">
+            <meta name="description" content="This zoom lecture will start in 5 mins">
+            <meta property="og:url" content="https://zb.robindecker.me/${req.params.id}">
+            <meta property="og:site_name" content="Zoomy">
+
+            <meta http-equiv="Refresh" content="0; url='${lectures[id].url}'">
         </head>
         <body>
             You should be redirected shortly.
