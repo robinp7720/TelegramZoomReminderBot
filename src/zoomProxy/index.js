43d799d3f3a81ede703933d859b406e27fb3c2e8
Zeyphros <robin@decker.cx>
2021-04-30 10:32:40 +0200

    Remove meta info

--- src/zoomProxy/index.js
+++ src/zoomProxy/index.js
@@ -4,7 +4,7 @@ import {allRoute} from './all';
 
 export const zoomProxyServer = restify.createServer({
     'name': 'ZoomBot'
-})
+});
 
 zoomProxyServer.get('/all', allRoute);
 
@@ -23,19 +23,8 @@ zoomProxyServer.get('/:id', (req, res, next) => {
     <html lang="en">
         <head>
             <meta charset="utf-8">
-                       
-            <meta name="referrer" content="origin-when-cross-origin">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1">
-            <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0">
+            
             <title>${lectures[id].name}</title>
-            <meta name="keywords" content="Learning, test, hello, fuckyou">
-            <meta name="robots" content="noindex,nofollow">
-            <meta property="og:type" content="activity">
-            <meta property="og:title" content="${lectures[id].name}">
-            <meta property="og:description" content="This zoom lecture will start in 5 mins">
-            <meta name="description" content="This zoom lecture will start in 5 mins">
-            <meta property="og:url" content="https://zb.robindecker.me/${req.params.id}">
-            <meta property="og:site_name" content="Zoomy">
 
             <meta http-equiv="Refresh" content="0; url='${lectures[id].url}'">
         </head>
@@ -45,4 +34,4 @@ zoomProxyServer.get('/:id', (req, res, next) => {
         </body>
     </html>
     `);
-})
+});
